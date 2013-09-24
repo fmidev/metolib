a18f03c83bcbe7c6138e6745e0a50974c7a392b1
unknown <karppinv@visa11k.pc.fmi.fi>
2013-09-24 12:45:03 +0300

    Repository information added and dependencies updated in package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,6 @@ module.exports = function(grunt) {
                 "boss" : true,
                 "eqnull" : true,
                 "node" : true,
-                "es5" : true,
                 "globals" : {
                     "it" : false,
                     "xit" : false,
